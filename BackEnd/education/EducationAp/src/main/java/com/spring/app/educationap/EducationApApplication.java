288228c8ed3ebaaa6d422dae65fd1e6159cd60ea
Hosam_Salah <hosamsalahsaad133@gmail.com>
2025-06-05 05:37:38 -0700

    first commit

---
+++ BackEnd/education/EducationAp/src/main/java/com/spring/app/educationap/EducationApApplication.java
@@ -0,0 +1,13 @@
+package com.spring.app.educationap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationApApplication.class, args);
+    }
+
+}
---
+++ BackEnd/education/EducationAp/src/test/java/com/spring/app/educationap/EducationApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app.educationap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
