21fa012cc171e5920b784f7ed3fd9f2b625f1373
Hosam_Salah <hosamsalahsaad133@gmail.com>
2025-06-10 15:41:04 -0700

    Complete components ( header - footer - profile - courses - exam - my courses ) Closes #13

--- FrontEnd/education/src/app/app.ts
+++ FrontEnd/education/src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './components/header/header';
+import {Footer} from './components/footer/footer';
+import {Profile} from './components/profile/profile';
+import {Exam} from './components/exam/exam';
+import {Courses} from './components/courses/courses';
+import {MyCourses} from './components/my-courses/my-courses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Profile, Exam, Courses, MyCourses],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
